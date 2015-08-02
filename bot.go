d807503a913619df44fe85189637767e7e0cd65e
Sam Jo <a1sams1a@gmail.com>
2015-08-02 22:33:46 +0900

    merge message files to msg.txt; add rand command

--- bot.go
+++ bot.go
@@ -1,33 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/tucnak/telebot"
 	"io/ioutil"
+	"math/rand"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
 
-var cmd_file_map map[string]string
-
-func get_file_content(cmd string) string {
-	elem, ok := cmd_file_map[cmd]
-
-	if ok {
-		return elem
-	}
-
-	data, err := ioutil.ReadFile("msg_" + cmd + ".txt")
-	if err != nil {
-		fmt.Println("Fatal(0x1): Unable to load a content file of " + cmd)
-		return "Fatal(0x1)"
-	}
-
-	str := string(data)
-	cmd_file_map[cmd] = str
-	return str
-}
+var msg_map map[string]string
 
 func parse(text string) (string, []string) {
 	arr := strings.Split(text, " ")
@@ -38,24 +23,40 @@ func parse(text string) (string, []string) {
 	return arr[0][1:], arr[1:]
 }
 
+func msgfmt(key string, args ...interface{}) string {
+	return fmt.Sprintf(msg_map[key], args...)
+}
+
 func handler_about(bot *telebot.Bot, msg telebot.Message, args []string) {
-	str := get_file_content("about")
-	bot.SendMessage(msg.Chat, str, nil)
+	bot.SendMessage(msg.Chat, msgfmt("about"), nil)
 }
 
 func handler_start(bot *telebot.Bot, msg telebot.Message, args []string) {
-	str := get_file_content("start")
-	bot.SendMessage(msg.Chat, str, nil)
+	bot.SendMessage(msg.Chat, msgfmt("start"), nil)
 }
 
 func handler_help(bot *telebot.Bot, msg telebot.Message, args []string) {
-	str := get_file_content("help")
-	bot.SendMessage(msg.Chat, str, nil)
+	bot.SendMessage(msg.Chat, msgfmt("help"), nil)
 }
 
 func handler_unknown(bot *telebot.Bot, msg telebot.Message, args []string) {
-	str := get_file_content("unknown")
-	bot.SendMessage(msg.Chat, str, nil)
+	bot.SendMessage(msg.Chat, msgfmt("unknown"), nil)
+}
+
+func handler_rand(bot *telebot.Bot, msg telebot.Message, args []string) {
+	if len(args) == 0 {
+		bot.SendMessage(msg.Chat, msgfmt("rand_noarg"), nil)
+		return
+	}
+
+	num, err := strconv.Atoi(args[0])
+	if err != nil || num <= 0 {
+		bot.SendMessage(msg.Chat, msgfmt("rand_invarg"), nil)
+		return
+	}
+
+	result := rand.Intn(num)
+	bot.SendMessage(msg.Chat, msgfmt("rand", num, result), nil)
 }
 
 func handler(bot *telebot.Bot, msg telebot.Message) {
@@ -67,6 +68,8 @@ func handler(bot *telebot.Bot, msg telebot.Message) {
 		handler_about(bot, msg, args)
 	case "help":
 		handler_help(bot, msg, args)
+	case "rand":
+		handler_rand(bot, msg, args)
 	default:
 		handler_unknown(bot, msg, args)
 	}
@@ -79,7 +82,19 @@ func main() {
 		return
 	}
 
-	cmd_file_map = make(map[string]string)
+	msg_map = make(map[string]string)
+	data, err := ioutil.ReadFile("msg.txt")
+	if err != nil {
+		fmt.Println("Fatal(0x1): Unable to read msg file")
+		return
+	}
+
+	err = json.Unmarshal(data, &msg_map)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("Fatal(0x2): Unable to parse msg file")
+		return
+	}
 
 	messages := make(chan telebot.Message)
 	bot.Listen(messages, 1*time.Second)
